223e4314b140a9098f5b86a391ad7c52a6a5642a
liudonghai <liudhzhyym@163.com>
2017-12-11 20:54:35 +0800

    update

---
+++ application/config/wechat.php
@@ -0,0 +1,12 @@
+<?php
+
+$config['wechat'] = array(
+    'token' => 'mars',
+    'encodingaeskey' => '',
+    'appid' => 'wx906ef255327eaa74',
+    'appsecret' => '87dc05c99d168869fd9ecd6f213196ef',
+    'debug' => FALSE,
+    // 默认为文件缓存
+    // memcached开启，配置目录为 /application/config/memcached.php
+    'memcached' => FALSE,
+);
